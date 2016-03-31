490b34c1cf598ad9460bb6cb09917513f5293907
Frazier Mork <fraziermork@gmail.com>
2016-03-31 01:40:04 -0700

    doesnt let you define multiple search bots yet

--- test/examples/search-bot-example.js
+++ test/examples/search-bot-example.js
@@ -13,7 +13,6 @@ let searchBotOptions = {
     }
   }
 };
-
 let searchBotDefinedCallback = function() {
   searchBotExample.bots.searchBots.backwards_bot.initialize((err, botName) => {
     if (err) {
@@ -23,13 +22,40 @@ let searchBotDefinedCallback = function() {
     }
   });
 };
-
-let stringToSearchFor = 'backwards';
-
+let stringToSearchFor = 'stuff';
 //going to make the content of my tweets be the text of their tweet except backwards
 let searchBotContent = function(tweet){
   console.log('searchBotContent called');
   return tweet.text.split('').reverse().join('');
 };
-
 searchBotExample.defineSearchBot(searchBotContent, stringToSearchFor, searchBotDefinedCallback, searchBotOptions);
+
+
+let searchBotOptions2 = {
+  botName: 'SDRAWKCAB_DNA',
+  callback: function(err, data, response){
+    if (err){
+      console.log('Error posting content', err);
+    } else {
+      console.log('Should have succesfully posted a tweet with text: ');
+      console.log(data.text);
+    }
+  }
+};
+
+let searchBotDefinedCallback2 = function() {
+  searchBotExample.bots.searchBots.backwards_bot.initialize((err, botName) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log( botName + ' started.');
+    }
+  });
+};
+let stringToSearchFor2 = 'dna';
+//going to make the content of my tweets be the text of their tweet except backwards
+let searchBotContent2 = function(tweet){
+  console.log('searchBotContent called');
+  return tweet.text.toUpperCase();
+};
+searchBotExample.defineSearchBot(searchBotContent2, stringToSearchFor2, searchBotDefinedCallback2, searchBotOptions2);
