ce08d5becd9b85091f5ddfa19322e7927bdfd92d
lwenke01 <lwenke@gmail.com>
2016-03-31 18:26:38 -0700

    got the timerbot test to work. Not built out as much as liked but checks that get statusese and post statuses work

--- test/timer-test.js
+++ test/timer-test.js
@@ -9,57 +9,37 @@ let chai      = require('chai');
 let expect    = chai.expect;
 let keys      = require(__dirname + '/../config/test-config1-lw.js');
 let t         = new Twit(keys);
-let counter = 0;
-let i =0;
-
-let timerBot;
-// var server    = rewire(__dirname + '/../server.js');
-// server.__set__('t', t);
 
 describe('defineTimerBot.js', function(){
+  describe('it should post statuses', function(){
+    before('setting up the status', (done) => {
+      // timerBot = new TimerBot();
+      setTimeout(done, 1200);
+      // done();
 
-
-describe('it should post statuses', function(){
-  before('setting up the status', (done) => {
-    // timerBot = new TimerBot();
-    setTimeout(done, 1200);
-    // done();
-
-  });
-  it('should let you get statuses to post', function(done) {
-    t.get('statuses/user_timeline', {status: 'Hello', screen_name: 'backwards_bot'}, function(err, data, response){
-      console.log('made request to get statuses');
-      console.log('DATA: ' + data);
-      expect(err).to.eql(undefined);
-      console.log(err);
-      // console.log(data.screen_name)
-      // expect(data.screeen_name).to.eql('backwards_bot');
-      // expect(TimberBot.dontStopOnError).to.eql(true);
-      expect(data).to.be.instanceof(Array);
-      // expect(this.interval).to.eql()
-      console.log('SCREEN NAME: ' + response.screen_name);
-      expect(response.statusCode).to.eql(200);
-      // expect(response.method).to.eql('GET');
-
-      // console.log(response);
-      // console.log();
-      // expect
-      counter =+ 1;
-      // expect(optionObj.botName).to.eql('timerBot' + counter);
-      // console.log('DATA IS _____________________________', data);
-      console.log('RESPONSE IS _____________________________', response.headers);
-      done();
     });
-    // t.post('statuses/update', {status: 'hey'}, function(err, data, response){
-    //   console.log('POST DATA: ' + data);
-    // });
-
-  // textReply();
-
+    it('should let you get statuses to post', function(done) {
+      t.get('statuses/user_timeline', {status: 'wookie3', screen_name: 'backwards_bot'}, function(err, data, response){
+        console.log('made request to get statuses');
+        expect(err).to.eql(undefined);
+        expect(response.statusCode).to.eql(200);
+        expect(data).to.be.instanceof(Array);
+        expect(data[0].user.name).to.eql('Reizarf Krom');
+        expect(data[0].user.id_str).to.eql('714197490480754688');
+        done();
+      });
+    });
+    it('should post the timestamp to the time',function(done){
+      t.post('statuses/update', {status: 'wookie9'}, function(err, data, response){
+        console.log('POST DATA______________ ', data);
+        // console.log('POST res______________ ', response[0]);
+        console.log('made request to post status');
+        expect(err).to.eql(undefined);
+        expect(response.statusCode).to.eql(200);
+        expect(data.text).to.eql('wookie9');
+        expect(data.user.name).to.eql('Reizarf Krom');
+        done();
+      });
+    });
   });
-
-});
-
-
-
 });
